df195220ae0cfa8634a8f68b509c37b870da65e9
Jack Matthews <jackhkmatthews@gmail.com>
2019-10-07 18:31:11 +0100

    feat(styled-components): add to project

---
+++ src/components/hello-world/hello-world.component.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./hello-world.styles";
+
+const HelloWorld: React.FC = () => {
+  return <S.HelloWorld>Hello World</S.HelloWorld>;
+};
+
+export default HelloWorld;
---
+++ src/components/hello-world/hello-world.styles.tsx
@@ -0,0 +1,5 @@
+import styled from "styled-components/macro";
+
+export const HelloWorld = styled.h1`
+  color: pink;
+`;
