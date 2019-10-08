eb85a084489d10e517f0429015dab8ca6e56d271
Jack Matthews <jackhkmatthews@gmail.com>
2019-10-08 09:06:43 +0100

    feat(Images): implement card grid

--- src/components/images/images.component.tsx
+++ src/components/images/images.component.tsx
@@ -1,26 +1,15 @@
 import React from "react";
 import * as S from "./images.styles";
-import { useSelector } from "react-redux";
-import { selectImages } from "../../redux/selectors";
-import { Link } from "react-router-dom";
-import { Image } from "../../redux/types";
-import Navbar from "../shared/navbar/navbar.component";
+import ImageCardGrid from "../shared/image-card-grid/image-card-grid.component";
+import MainLayout from "../shared/main-layout/main-layout.component";
 
 const Images: React.FC = () => {
-  const images = useSelector(selectImages);
-  const listItems = images.map((image: Image) => {
-    const src = `https://storage.googleapis.com/${image.imgSrc}`;
-    return (
-      <Link to={`/images/${image.slug}`} key={src}>
-        <img src={src} alt={image.imgAltText} />
-      </Link>
-    );
-  });
   return (
     <S.Images>
-      <Navbar />
-      <S.Title>Images</S.Title>
-      {listItems}
+      <S.StyledNavbar />
+      <MainLayout>
+        <ImageCardGrid />
+      </MainLayout>
     </S.Images>
   );
 };
--- src/components/images/images.styles.tsx
+++ src/components/images/images.styles.tsx
@@ -1,9 +1,8 @@
 import styled from "styled-components/macro";
+import Navbar from "../shared/navbar/navbar.component";
 
-export const Images = styled.div`
-  background-color: pink;
-`;
+export const Images = styled.div``;
 
-export const Title = styled.h1`
-  color: white;
+export const StyledNavbar = styled(Navbar)`
+  margin-bottom: ${props => props.theme.spacing.navbarMarginBottom}px;
 `;
