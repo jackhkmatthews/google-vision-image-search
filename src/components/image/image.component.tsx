33a122af267ac9eb5b9c5c6402b83d2c3da45256
Jack Matthews <jackhkmatthews@gmail.com>
2019-10-10 18:08:05 +0100

    feat(Image): add download button

--- src/components/image/image.component.tsx
+++ src/components/image/image.component.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import * as S from "./image.styles";
 import { useSelector } from "react-redux";
-import { selectImages } from "../../redux/selectors";
+import { selectImages, selectImgDownload } from "../../redux/selectors";
 import Navbar from "../shared/navbar/navbar.component";
-import ImageEditor from "../shared/image-editor/image-editor.component";
 import MainLayout from "../shared/main-layout/main-layout.component";
 
 export interface Props {
@@ -18,13 +17,21 @@ const Image: React.FC<Props> = ({ match }: Props) => {
   const image = useSelector(selectImages).filter(
     image => image.slug === match.params.slug
   )[0];
+  const imgDownload = useSelector(selectImgDownload);
   const src = `https://storage.googleapis.com/${image.imgSrc}`;
   return (
     <S.Image>
       <Navbar />
       <S.StyledImageToolbar />
       <MainLayout>
-        <ImageEditor src={src} />
+        {imgDownload ? (
+          <S.Download>
+            <S.DownloadLink href={imgDownload} download={image.imgSrc}>
+              Download
+            </S.DownloadLink>
+          </S.Download>
+        ) : null}
+        <S.StyledImageEditor src={src} />
       </MainLayout>
     </S.Image>
   );
--- src/components/image/image.styles.tsx
+++ src/components/image/image.styles.tsx
@@ -1,8 +1,27 @@
 import styled from "styled-components/macro";
 import ImageToolbar from "../shared/image-toolbar/image-toolbar.component";
+import ImageEditor from "../shared/image-editor/image-editor.component";
 
 export const Image = styled.div``;
 
 export const StyledImageToolbar = styled(ImageToolbar)`
   margin-bottom: ${props => props.theme.spacing.navbarMarginBottom}px;
+`;
+
+export const Download = styled.div`
+  display: flex;
+  justify-content: space-around;
+`;
+
+export const DownloadLink = styled.a`
+  display: inline-block;
+  margin: 0 auto;
+  padding: 10px;
+  text-decoration: none;
+  color: white;
+  background-color: green;
+`;
+
+export const StyledImageEditor = styled(ImageEditor)`
+  margin-top: -20px;
 `;
