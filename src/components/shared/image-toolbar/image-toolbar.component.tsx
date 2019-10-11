a216e2bf4993de26a8f3c6a150dee9aed000a530
Jack Matthews <jackhkmatthews@gmail.com>
2019-10-11 19:35:29 +0100

    feat(ImageToolbar): update editing state

--- src/components/shared/image-toolbar/image-toolbar.component.tsx
+++ src/components/shared/image-toolbar/image-toolbar.component.tsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import * as S from "./image-toolbar.styles";
-import MainLayout from "../main-layout/main-layout.component";
+import { useSelector, useDispatch } from "react-redux";
+import { selectEditing } from "../../../redux/selectors";
+import { setEditing, setCanvasClicks } from "../../../redux/actions";
 
 interface Props {
   className?: string;
 }
 
 const ImageToolbar: React.FC<Props> = ({ className }: Props) => {
+  const editing = useSelector(selectEditing);
+  const dispatch = useDispatch();
+  const handleClick = (): void => {
+    const leavingEditMode = editing;
+    if (leavingEditMode) {
+      dispatch(setCanvasClicks([]));
+    }
+    dispatch(setEditing(!editing));
+  };
+  useEffect(() => {
+    return function cleanUp(): void {
+      dispatch(setEditing(false));
+    };
+  }, [dispatch]);
   return (
     <S.ImageToolbar className={className}>
-      <MainLayout>
+      <S.StyledMainLayout>
         <S.Link to={"/images"}>
           <S.Title>Back to images</S.Title>
         </S.Link>
-      </MainLayout>
+        <S.CTA red={editing} onClick={handleClick}>
+          {editing ? "Cancel" : "Edit"}
+        </S.CTA>
+      </S.StyledMainLayout>
     </S.ImageToolbar>
   );
 };
--- src/components/shared/image-toolbar/image-toolbar.styles.tsx
+++ src/components/shared/image-toolbar/image-toolbar.styles.tsx
@@ -1,11 +1,22 @@
 import styled from "styled-components/macro";
 import { NavLink } from "react-router-dom";
+import MainLayout from "../main-layout/main-layout.component";
+
+interface Props {
+  red: boolean;
+}
 
 export const ImageToolbar = styled.div`
   background-color: ${props => props.theme.colors.grey2};
   display: flex;
   align-items: center;
   justify-content: space-between;
+`;
+
+export const StyledMainLayout = styled(MainLayout)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 export const Link = styled(NavLink)`
@@ -16,8 +27,13 @@ export const Title = styled.h5`
   color: ${props => props.theme.colors.black};
 `;
 
-export const Placeholder = styled.div`
+export const CTA = styled.button<Props>`
   display: inline-block;
-  width: 10px;
-  height: 10px;
+  padding: 10px;
+  text-decoration: none;
+  background-color: ${props =>
+    props.red ? props.theme.colors.red : props.theme.colors.white};
+  color: ${props =>
+    props.red ? props.theme.colors.white : props.theme.colors.black};
+  border: none;
 `;
